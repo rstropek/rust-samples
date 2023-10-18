04988654312deba1faa6623ac6f17ac396b294ef
Rainer <rainer@software-architects.at>
2023-10-18 09:06:01 +0200

    Axum

--- axum_intro/src/main.rs
+++ axum_intro/src/main.rs
@@ -1,21 +1,39 @@
-use axum::{routing::get, Router, extract::{Path, Query}, Json, response::IntoResponse, http::StatusCode};
+use std::sync::Arc;
+
+use axum::{routing::get, Router, extract::{Path, Query, State}, Json, response::{IntoResponse, Response, Html}, http::StatusCode};
 use rand::Rng;
 use serde::{Serialize, Deserialize};
+use thiserror::Error;
+use tokio::{sync::RwLock, fs::File, io::AsyncReadExt};
+
+#[derive(Default, Clone)]
+struct AppState {
+    numbers: Vec<i32>,
+}
+
+// Note that you can use trait objects for shared state, too. This is useful
+// for e.g. mock objects in unit tests. Sample for trait object state:
+// https://github.com/tokio-rs/axum/blob/8854e660e9ab07404e5bb8e30b92311d3848de05/examples/error-handling-and-dependency-injection/src/main.rs#L124
+type AppStateType = Arc<RwLock<AppState>>;
 
 // Note that Axum uses tokio as its runtime, so we need to use the #[tokio::main]
 #[tokio::main]
 async fn main() {
+
     // Learn more about routing at https://docs.rs/axum/latest/axum/index.html#routing
     let app = Router::new()
         .route("/", get(hello_world).post(|| async { "Hello, World ✉️!"}))
+        .route("/greet/:name", get(greet_path))
+        .route("/greet", get(greet_query).post(greet_body))
+        .route("/lookup/:number", get(look_it_up))
+        .route("/random", get(return_something_random))
+        .merge(numbers_management())
+        .with_state(AppStateType::default())
         // Let's add additional routes. Note that we can structure complex
         // routing hierarchies using methods like merge and nest.
         .merge(pingpong())
         .nest("/kingkong", kingkong())
-        .route("/greet/:name", get(greet_path))
-        .route("/greet", get(greet_query).post(greet_body))
-        .route("/lookup/:number", get(look_it_up))
-        .route("/random", get(return_something_random));
+        .merge(poem());
 
     // Run the server on port 3000. In practice, make settings like that
     // configurable (e.g. using clap).
@@ -105,4 +123,84 @@ async fn return_something_random() -> impl IntoResponse {
         1 => StatusCode::NOT_IMPLEMENTED.into_response(),
         _ => Json(42).into_response(),
     }
+}
+
+fn numbers_management() -> Router<AppStateType> {
+    // State is another extractor. It can be used to extract shared state.
+    // Read more at https://docs.rs/axum/latest/axum/index.html#using-the-state-extractor
+    // .merge(numbers_management())
+    // .with_state(Arc::new(RwLock::new(AppState::default())))
+    async fn get_numbers(State(state): State<AppStateType>) -> impl IntoResponse {
+        Json(state.read().await.numbers.clone())
+    }
+
+    async fn add_number(State(state): State<AppStateType>, Json(new_number): Json<i32>) -> impl IntoResponse {
+        let mut writable_state = state.write().await;
+        writable_state.numbers.push(new_number);
+        Json(writable_state.numbers.clone())
+    }
+
+    Router::new()
+        .route("/numbers", get(get_numbers).post(add_number))
+}
+
+fn poem() -> Router {
+    // Possible errors that can occur when reading poem from file.
+    // Note that this uses thiserror.
+    #[derive(Error, Debug)]
+    pub enum PoemError {
+        #[error("error accessing file")]
+        FileAccess(#[from] tokio::io::Error),
+        #[error("error parsing yaml")]
+        YamlParse(#[from] serde_yaml::Error),
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Poem {
+        pub title: String,
+        pub text: String,
+    }
+
+    // Let's write a helper method that reads a poem from a file.
+    async fn read_from_file(path: &str) -> Result<Poem, PoemError> {
+        let mut contents = String::new();
+        File::open(path).await?.read_to_string(&mut contents).await?;
+        Ok(serde_yaml::from_str(&contents)?)
+    }
+
+    // Implement IntoResponse for our error type.
+    impl IntoResponse for PoemError {
+        fn into_response(self) -> Response {
+            let (status, error_message) = match self {
+                PoemError::FileAccess(ioe) => {
+                    (StatusCode::INTERNAL_SERVER_ERROR, format!("Error while accessing file: {ioe}"))
+                },
+                PoemError::YamlParse(ye) => {
+                    (StatusCode::INTERNAL_SERVER_ERROR, format!("Error in YMAL file: {ye}"))
+                },
+            };
+    
+            let body = Json(error_message);
+                (status, body).into_response()
+        }
+    }
+
+    // Handler turning our poem into HTML.
+    async fn get_poem() -> Result<Html<String>, PoemError> {
+        let poem = read_from_file("poem.yaml").await?;
+        Ok(Html(format!(r#"
+            <html>
+                <head>
+                    <title>{}</title>
+                </head>
+                <body>
+                    <h1>{}</h1>
+                    <pre>{}</pre>
+                </body>
+            </html>
+        "#, poem.title, poem.title, poem.text)))
+    }
+
+    Router::new()
+        .route("/poem", get(get_poem))
 }
